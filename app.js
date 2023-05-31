7b737609f6b95c57157397aa245dacbf62acbb50
coritac <brendaochoa408@gmail.com>
2023-05-30 23:14:28 -0300

    Reforme el archivo app.js y pueden verse las imagenes

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
 
 
 
