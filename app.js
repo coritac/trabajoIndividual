12c69571ade5e2d19c6707615dc17e7081ff992b
coritac <brendaochoa408@gmail.com>
2023-08-16 23:48:34 -0300

    Pusheando el trabajo casi terminado

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
+app.get('/login', (req,res) =>{
+    res.sendFile((__dirname + '/views/login.html')); 
+});
+
+app.get('/register', (req,res) =>{
+    res.sendFile((__dirname + '/views/register.html')); 
+});
+
 
 
 
